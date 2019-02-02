3b6af459f2ac108c86f9cf72146020e4c7d41b53
ToMMaaY <thuynh629@gmail.com>
2019-02-02 15:29:17 -0800

    created the items schema

--- data/migrations/20190202150135_users.js
+++ data/migrations/20190202150135_users.js
@@ -1,14 +1,13 @@
 exports.up = function(knex, Promise) {
    return knex.schema.createTable('users', tbl => {
-      tbl.increments();
-
+      tbl.increments('userId');
       tbl.string('username')
          .unqiue()
          .notNullable();
       tbl.string('firstName').notNullable();
       tbl.string('lastName').notNullable();
       tbl.string('email').notNullable();
-      tbl.integer('phone');
+      tbl.integer('phone', 10);
    });
 };
 
---
+++ data/migrations/20190202151434_items.js
@@ -0,0 +1,26 @@
+exports.up = function(knex, Promise) {
+   return knex.schema.createTable('items', tbl => {
+      tbl.increments('itemId');
+      tbl.integer('owner')
+         .notNullable()
+         .unsigned()
+         .references('userId')
+         .inTable('users');
+      tbl.string('title').notNullable();
+      tbl.text('description', longtext).notNullable();
+      tbl.string('brand').notNullable();
+      tbl.string('model');
+      tbl.float('dailyPrice').notNullable();
+      tbl.float('weeklyPrice').notNullable();
+      tbl.boolean('available').notNullable();
+      tbl.integer('renter')
+         .notNullable()
+         .unsigned()
+         .references('userId')
+         .inTable('users');
+   });
+};
+
+exports.down = function(knex, Promise) {
+   return knex.schema.dropTableIfExists('items');
+};
