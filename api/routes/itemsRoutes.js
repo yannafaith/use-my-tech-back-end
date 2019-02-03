ebba30557852e5cbabe56fbcb8ce4a6700a81d2b
ToMMaaY <thuynh629@gmail.com>
2019-02-02 17:08:47 -0800

    hooked up the get item by id endpoint

--- api/routes/itemsRoutes.js
+++ api/routes/itemsRoutes.js
@@ -12,4 +12,20 @@ route.get('/', async (req, res) => {
    }
 });
 
+route.get('/:id', async (req, res) => {
+   const { id } = req.params;
+
+   try {
+      const item = await db('items')
+         .where({ itemId: id })
+         .first();
+
+      !item
+         ? res.status(404).json({ error: 'Item does not exist' })
+         : res.status(200).json(item);
+   } catch (err) {
+      res.status(500).json({ error: 'Unable to fetch the item' });
+   }
+});
+
 module.exports = route;
