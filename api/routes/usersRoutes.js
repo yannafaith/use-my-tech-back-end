d1eeffc73228564930fa4756732c70dacfc14f74
ToMMaaY <thuynh629@gmail.com>
2019-02-02 16:26:26 -0800

    hooked up the get user by id endpoint

--- api/routes/usersRoutes.js
+++ api/routes/usersRoutes.js
@@ -12,4 +12,19 @@ route.get('/', async (req, res) => {
    }
 });
 
+route.get('/:id', async (req, res) => {
+   const { id } = req.params;
+   const user = await db('users')
+      .where({ userId: id })
+      .first();
+
+   try {
+      !user
+         ? res.status(404).json({ error: 'User does not exist' })
+         : res.status(200).json(user);
+   } catch (err) {
+      res.status(500).json({ error: 'Unable to fetch the user' });
+   }
+});
+
 module.exports = route;
