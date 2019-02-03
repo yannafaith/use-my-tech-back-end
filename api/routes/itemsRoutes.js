79645c1e0adb6c2355cfca03d4199cd2911873be
ToMMaaY <thuynh629@gmail.com>
2019-02-02 17:07:15 -0800

    hooked up the get items endpoint

--- api/routes/itemsRoutes.js
+++ api/routes/itemsRoutes.js
@@ -3,4 +3,13 @@ const route = express.Router();
 
 const db = require('../../data/dbConfig');
 
+route.get('/', async (req, res) => {
+   try {
+      const items = await db('items');
+      res.status(200).json(items);
+   } catch (err) {
+      res.status(500).json({ message: 'Could not retrieve the list of items' });
+   }
+});
+
 module.exports = route;
