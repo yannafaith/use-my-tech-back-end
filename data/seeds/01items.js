fd998030905157bebd3d9a4bc30001d74087d49d
ToMMaaY <thuynh629@gmail.com>
2019-02-06 08:41:56 -0800

    changed truncate to del for postgres

--- data/seeds/01items.js
+++ data/seeds/01items.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex, Promise) {
    // Deletes ALL existing entries
    return knex('items')
-      .truncate()
+      .del()
       .then(function() {
          // Inserts seed entries
          return knex('items').insert([
