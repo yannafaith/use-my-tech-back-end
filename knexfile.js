6f1f52c107151a794c88414668941a609407fbd8
ToMMaaY <thuynh629@gmail.com>
2019-02-06 08:37:04 -0800

    changing to postgres

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
 // Update with your config settings.
+const dbConnection = process.env.DATABASE_URL;
 
 module.exports = {
    development: {
@@ -13,5 +14,22 @@ module.exports = {
       seeds: {
          directory: './data/seeds',
       },
+   },
+
+   production: {
+      client: 'pg',
+      connection: dbConnection,
+      pool: {
+         min: 2,
+         max: 10,
+      },
+      migrations: {
+         tableName: 'knex_migrations',
+         directory: './data/migrations',
+      },
+      seeds: {
+         directory: './data/seeds',
+      },
+      useNullAsDefault: true,
    },
 };
