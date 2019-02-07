b61af2b1e5b76ddaba29ac146d40a6fc4711a3fe
ToMMaaY <thuynh629@gmail.com>
2019-02-06 18:47:33 -0800

    added more seed data

--- data/seeds/00users.js
+++ data/seeds/00users.js
@@ -50,7 +50,7 @@ exports.seed = function(knex, Promise) {
                firstname: 'Kayanna',
                lastname: 'Chandler',
                email: 'yanna@gmail.com',
-               phone: 4152335576,
+               phone: 4082335576,
                thumbnail: faker.image.avatar()
             },
             {
@@ -60,6 +60,69 @@ exports.seed = function(knex, Promise) {
                lastname: 'Jones',
                email: 'kelly@gmail.com',
                phone: 4152335576,
+               thumbnail: faker.image.avatar()
+            },
+            {
+               username: faker.internet.username(),
+               password: bcrypt.hashSync('password', 11),
+               firstname: faker.name.firstName(),
+               lastname: faker.name.lastName(),
+               email: faker.internet.email(),
+               phone: 8282435576,
+               thumbnail: faker.image.avatar()
+            },
+            {
+               username: faker.internet.username(),
+               password: bcrypt.hashSync('password', 11),
+               firstname: faker.name.firstName(),
+               lastname: faker.name.lastName(),
+               email: faker.internet.email(),
+               phone: 7246245423,
+               thumbnail: faker.image.avatar()
+            },
+            {
+               username: faker.internet.username(),
+               password: bcrypt.hashSync('password', 11),
+               firstname: faker.name.firstName(),
+               lastname: faker.name.lastName(),
+               email: faker.internet.email(),
+               phone: 6625333687,
+               thumbnail: faker.image.avatar()
+            },
+            {
+               username: faker.internet.username(),
+               password: bcrypt.hashSync('password', 11),
+               firstname: faker.name.firstName(),
+               lastname: faker.name.lastName(),
+               email: faker.internet.email(),
+               phone: 33451355317,
+               thumbnail: faker.image.avatar()
+            },
+            {
+               username: faker.internet.username(),
+               password: bcrypt.hashSync('password', 11),
+               firstname: faker.name.firstName(),
+               lastname: faker.name.lastName(),
+               email: faker.internet.email(),
+               phone: 2434274434,
+               thumbnail: faker.image.avatar()
+            },
+            {
+               username: faker.internet.username(),
+               password: bcrypt.hashSync('password', 11),
+               firstname: faker.name.firstName(),
+               lastname: faker.name.lastName(),
+               email: faker.internet.email(),
+               phone: 4534513452,
+               thumbnail: faker.image.avatar()
+            },
+            {
+               username: faker.internet.username(),
+               password: bcrypt.hashSync('password', 11),
+               firstname: faker.name.firstName(),
+               lastname: faker.name.lastName(),
+               email: faker.internet.email(),
+               phone: 6426682425,
                thumbnail: faker.image.avatar()
             },
          ]);
