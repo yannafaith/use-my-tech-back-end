c7204f361c885fa3931e32879193ee864e93b199
ToMMaaY <thuynh629@gmail.com>
2019-02-06 21:29:37 -0800

    updated cors config

--- config/middleware.js
+++ config/middleware.js
@@ -7,5 +7,5 @@ module.exports = server => {
    server.use(helmet());
    server.use(express.json());
    server.use(morgan('short'));
-   server.use(cors());
+   server.use(cors({credentials: true, origin: true}));
 };
